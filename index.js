e78db1e111b4505c2c2c72a7216cf2610a8251ac
Javier <Familia@MacBook-Air-de-Javier.local>
2019-01-10 12:04:09 +0100

    connect mongo, create new image with mongo

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
 
 const bodyParser   = require('body-parser');
 const express      = require('express');
 const app = express();
 const messages = require('./messages')
+const mongoose     = require('mongoose');
 
+
+mongoose
+  .connect('mongodb://localhost:27017/message_app', {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
 
 app.use(bodyParser.json());
 
