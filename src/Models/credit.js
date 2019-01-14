dc0aca6f9455dedd30d3aff7ac1053e80ab4dab7
Javier <Familia@MacBook-Air-de-Javier.local>
2019-01-14 12:21:30 +0100

    add model credit

---
+++ src/Models/credit.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const creditSchema = new Schema({
+  credit: Number,
+  
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+const Credit = mongoose.model('Credit', creditSchema);
+module.exports = Credit;
